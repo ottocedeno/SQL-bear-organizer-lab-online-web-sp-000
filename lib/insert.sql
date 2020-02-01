bef2626e3536b16c79e1404cdd00063773f258bd
Otto Cedeno <otto@ottostacos.com>
2020-02-01 05:06:12 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+  ("Mr. Chocolate", 42, "Male", "Brown", "Chill", 1),
+  ("Rowdy", 7, "Male", "White", "Energetic", 1),
+  ("Tabitha", 20, "Female", "Polar", "Aloof", 0),
+  ("Seargent Brown", 50, "Male", "Brown", "Aloof", 0),
+  ("Melissa", 10, "Female", "Golden", "Aggresive", 1),
+  ("Grinch", 19, "Male", "Black", "Aggresive", 1),
+  ("Wendy", 60, "Female", "Polar", "Chill", 1),
+  (NULL, 27, "Male", "Black", "Aggressive", "true");
